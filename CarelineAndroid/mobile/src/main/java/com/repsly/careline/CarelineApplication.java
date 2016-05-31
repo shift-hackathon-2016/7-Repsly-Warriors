988ff22a915f119e1bce676d7b4c78b1c5117688
Tomislav Šulc <tomislav.sulc@repsly.com>
2016-05-31 12:31:22 +0200

    Initial database structure
    Also:
    -added butterknife
    -added dexcount

---
+++ CarelineAndroid/mobile/src/main/java/com/repsly/careline/CarelineApplication.java
@@ -0,0 +1,17 @@
+package com.repsly.careline;
+
+import android.app.Application;
+
+import com.repsly.careline.database.DbHelper;
+
+/**
+ * Created by tosulc on 31.05.2016..
+ */
+public class CarelineApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
