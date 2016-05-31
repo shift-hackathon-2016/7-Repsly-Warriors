55a637ad20a9077ddca6a215d7a60854d93e0777
Alen Huskanović <huskanovicalen@gmail.com>
2016-05-31 13:00:20 +0200

    Receiver home header established

--- CarelineAndroid/mobile/src/main/java/com/repsly/careline/activities/HomeGiverActivity.java
+++ CarelineAndroid/mobile/src/main/java/com/repsly/careline/activities/HomeGiverActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.repsly.careline.R;
+
 /**
  * Created by Alen on 31.5.2016..
  */
@@ -12,6 +14,7 @@ public class HomeGiverActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_giver_activity);
 
     }
 }
--- CarelineAndroid/mobile/src/main/java/com/repsly/careline/activities/HomeReceiverActivity.java
+++ CarelineAndroid/mobile/src/main/java/com/repsly/careline/activities/HomeReceiverActivity.java
@@ -1,8 +1,19 @@
 package com.repsly.careline.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.repsly.careline.R;
+
 /**
  * Created by Alen on 31.5.2016..
  */
-public class HomeReceiverActivity {
+public class HomeReceiverActivity extends AppCompatActivity{
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_receiver_activity);
+    }
 }
