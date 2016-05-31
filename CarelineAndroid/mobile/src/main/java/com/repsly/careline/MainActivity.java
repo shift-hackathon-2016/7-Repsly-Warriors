a4f6988ee6b10dacf02cdef1a36db10c2b20e5f1
Tomislav Šulc <tomislav.sulc@repsly.com>
2016-05-31 10:28:27 +0200

    Android app added

---
+++ CarelineAndroid/mobile/src/main/java/com/repsly/careline/MainActivity.java
@@ -0,0 +1,13 @@
+package com.repsly.careline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ CarelineAndroid/wear/src/main/java/com/repsly/careline/MyStubBroadcastActivity.java
@@ -0,0 +1,22 @@
+package com.repsly.careline;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("com.repsly.careline.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
