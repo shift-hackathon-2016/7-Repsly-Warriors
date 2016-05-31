c0b3f9ced5fc8a0325346066264a4306be203a61
Tomislav Šulc <tomislav.sulc@repsly.com>
2016-05-31 14:14:13 +0200

    Retrofit APIs additions

---
+++ CarelineAndroid/mobile/src/main/java/com/repsly/careline/model/MedicineConfirmation.java
@@ -0,0 +1,11 @@
+package com.repsly.careline.model;
+
+/**
+ * Created by tosulc on 31.05.2016..
+ */
+public class MedicineConfirmation {
+
+    public String scheduleRowId;
+    public String dateTime;
+
+}
--- CarelineAndroid/mobile/src/main/java/com/repsly/careline/retrofit/interfaces/ApiCarelineInterface.java
+++ CarelineAndroid/mobile/src/main/java/com/repsly/careline/retrofit/interfaces/ApiCarelineInterface.java
@@ -1,11 +1,12 @@
 package com.repsly.careline.retrofit.interfaces;
 
+import com.repsly.careline.model.MedicineConfirmation;
 import com.repsly.careline.model.User;
 
-import java.util.List;
-
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 /**
@@ -16,7 +17,12 @@ public interface ApiCarelineInterface  {
     @GET
     Call<User> getUserData(@Path("userId") String userId);
 
-    /*@GET("users/{user}/repos")
-    Call<List<OneModel>> listRepos(@Path("user") String user);*/
+    @GET
+    Call<User> getScheduleForMobileUser(); //getting schedule and schedule items
 
+    @POST("/api/Schedule/MedicineConfirmation")
+    Call<Void> sendMedicineConformation(@Body MedicineConfirmation mc);
+
+    @POST()
+    Call<Void> sendCurrentLocation(); //TODO implement
 }
