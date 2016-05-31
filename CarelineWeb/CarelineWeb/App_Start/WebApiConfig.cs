dd6135632f5bddd7800613b6a7a393eec2e825d1
bmasnec <boris.masnec@repsly.com>
2016-05-31 10:32:45 +0200

    Web api project rename

--- CarelineWeb/CarelineWeb/App_Start/WebApiConfig.cs
+++ CarelineWeb/CarelineWeb/App_Start/WebApiConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
-namespace CarelineWeb
+namespace CarelineWebAPI
 {
     public static class WebApiConfig
     {
--- CarelineWeb/CarelineWeb/Properties/AssemblyInfo.cs
+++ CarelineWeb/CarelineWeb/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CarelineWeb")]
+[assembly: AssemblyTitle("CarelineWebAPI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CarelineWeb")]
+[assembly: AssemblyProduct("CarelineWebAPI")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
