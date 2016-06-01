9065e17a8f6fa96d05875f977e7be058a645bb4f
Tomislav Šulc <tomislav.sulc@repsly.com>
2016-06-01 07:59:56 +0200

    Sending tracking event
    -when movement timer comes, we send when did the mobile last moved
    :money_with_wings:

--- CarelineAndroid/mobile/src/main/java/com/repsly/careline/MovementReceiver.java
+++ CarelineAndroid/mobile/src/main/java/com/repsly/careline/MovementReceiver.java
@@ -5,6 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+import com.repsly.careline.helpers.Constants;
+import com.repsly.careline.helpers.DateTimeUtil;
+import com.repsly.careline.model.TrackingEvent;
+import com.repsly.careline.retrofit.ApiCarelineImpl;
+import com.tumblr.remember.Remember;
+
+import java.util.Date;
+
 /**
  * Created by tosulc on 31.05.2016..
  */
@@ -12,6 +20,17 @@ public class MovementReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        if (CarelineApplication.lastGpsLocation != null) {
+            ApiCarelineImpl service = new ApiCarelineImpl().buildInterceptor()
+                                                           .addAuthHeader(
+                                                                   Remember.getString(
+                                                                           Constants.LOGIN_DATA,
+                                                                           ""));
+            service.sendUserTracking(
+                    new TrackingEvent(4, Remember.getString(Constants.MOTION_TIME, ""),
+                                      CarelineApplication.lastGpsLocation.getLatitude(),
+                                      CarelineApplication.lastGpsLocation.getLongitude(),
+                                      ""));
+        }
     }
 }
