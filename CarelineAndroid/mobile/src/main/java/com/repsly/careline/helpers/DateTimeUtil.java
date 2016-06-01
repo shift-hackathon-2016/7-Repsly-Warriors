8f3e7b5a12d00de1f97658fda42de6fa4161fc30
Alen Huskanović <huskanovicalen@gmail.com>
2016-06-01 04:14:52 +0200

    CareReceiver save and show map on click

--- CarelineAndroid/mobile/src/main/java/com/repsly/careline/helpers/DateTimeUtil.java
+++ CarelineAndroid/mobile/src/main/java/com/repsly/careline/helpers/DateTimeUtil.java
@@ -12,6 +12,17 @@ public class DateTimeUtil {
 
     public final static String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 
+    public final static String DATE_FORMAT_SHOW = "EEE, MMM d, HH:mm";
+
+
+    public static String toShowDate(Date d) {
+        if (d == null) {
+            d = new Date(System.currentTimeMillis());
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_SHOW, Locale.US);
+        return sdf.format(d);
+    }
+
     public static String toISODate(Date d) {
         if (d == null) {
             d = new Date(System.currentTimeMillis());
--- CarelineAndroid/mobile/src/main/java/com/repsly/careline/model/CareReceiver.java
+++ CarelineAndroid/mobile/src/main/java/com/repsly/careline/model/CareReceiver.java
@@ -2,11 +2,21 @@ package com.repsly.careline.model;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import com.repsly.utils.lib.list.ListItem;
 
 /**
  * Created by tosulc on 01.06.2016..
  */
-public class CareReceiver {
+public class CareReceiver extends ListItem {
+
+    @SerializedName("UserId")
+    @Expose
+    public int userId;
+
+    @SerializedName("LastMovementDateTime")
+    @Expose
+    public String lastMovement;
+
     @SerializedName("Name")
     @Expose
     public String name;
@@ -20,4 +30,51 @@ public class CareReceiver {
     @Expose
     public String note;
 
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getLastMovement() {
+        return lastMovement;
+    }
+
+    public void setLastMovement(String lastMovement) {
+        this.lastMovement = lastMovement;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 }
